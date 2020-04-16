b87df13c84bec82432d13c53f838141e0d9375e4
Mateusz Popielarz <mateusz.popielarz@fortedigital.no>
2020-04-16 20:34:03 +0200

    BE: Migration for Messages

---
+++ Agh.eSzachy/Migrations/20200416183330_Message.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agh.eSzachy.Migrations
+{
+    public partial class Message : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_ClientIdId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ClientIdId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "ClientIdId",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientId",
+                table: "Messages",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ClientId",
+                table: "Messages",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_ClientId",
+                table: "Messages",
+                column: "ClientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_ClientId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ClientId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientIdId",
+                table: "Messages",
+                type: "varchar(95) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ClientIdId",
+                table: "Messages",
+                column: "ClientIdId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_ClientIdId",
+                table: "Messages",
+                column: "ClientIdId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
