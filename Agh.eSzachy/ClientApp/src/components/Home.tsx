a81cac63706677523cc913ffb38ae1455f41d109
Damian Bucki <damian.bucki@onet.pl>
2020-05-07 19:02:23 +0200

    init ui

--- Agh.eSzachy/ClientApp/src/components/Home.tsx
+++ Agh.eSzachy/ClientApp/src/components/Home.tsx
@@ -3,32 +3,34 @@ import { HubConnectionBuilder, HubConnectionState } from "@aspnet/signalr";
 import { RoomHub, Room } from "../Api";
 import authService from "./api-authorization/AuthorizeService";
 
-const initial = {
-  hub: {} as RoomHub,
-  message: "",
-  room: "",
-};
-
 export const Home = () => {
   authService.getAccessToken();
 
-  const [state, setState] = useState(initial);
   const [dataState, setDataState] = useState([] as Room[]);
+
+  const [hub, setHub] = useState<RoomHub>({} as RoomHub);
+  const [message, setMessage] = useState<string>("");
+  const [room, setRoom] = useState<string>("");
+
+  const [roomList, setRoomList] = useState<string[]>([] as string[]);
 
   useEffect(() => {
     (async () => {
-      if (state.hub instanceof RoomHub) {
+      if (hub instanceof RoomHub) {
         return;
       }
+
       const token = await authService.getAccessToken();
       if (typeof token !== "string") {
         throw new Error();
       }
+
       const c = new HubConnectionBuilder()
         .withUrl("/room", { accessTokenFactory: () => token })
         .build();
-      const hub = new RoomHub(c);
-      hub.registerCallbacks({
+
+      const roomHub = new RoomHub(c);
+      roomHub.registerCallbacks({
         refresh: (r) => {
           setDataState(r);
         },
@@ -37,78 +39,40 @@ export const Home = () => {
         },
         send: (m) => {},
       });
-      if (c.state === HubConnectionState.Disconnected) {
-        await c.start();
-        // debugger;
-        setState({ room: "", message: "", hub: hub });
+
+      switch (c.state) {
+        case HubConnectionState.Disconnected:
+          await c.start();
+
+          setHub(roomHub);
+          setMessage("");
+          setRoom("");
+          break;
+
+        case HubConnectionState.Connected:
+          const roomList = await roomHub.getAllRooms();
+
+          setRoomList(roomList);
+          break;
       }
     })();
-  }, [state.hub, setDataState]);
+  }, [hub, setDataState]);
 
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <div>{JSON.stringify(dataState)}</div>
+    <>
+      {/* <div>{JSON.stringify(dataState)}</div> */}
+      <div>{JSON.stringify(roomList)}</div>
       Message
-      <input
-        onChange={(x) => setState({ ...state, message: x.target.value })}
-        type="text"
-      />
+      <input onChange={(x) => setMessage(x.target.value)} type="text" />
       Room
-      <input
-        onChange={(x) => setState({ ...state, room: x.target.value })}
-        type="text"
-      />
-      <button onClick={(x) => state.hub.send(state.room, state.message)}>
-        send
-      </button>
-      <button onClick={(x) => state.hub.create(state.room)}>create</button>
-      <button onClick={(x) => state.hub.join(state.room)}>join</button>
-      <button onClick={(x) => state.hub.leave(state.room)}>left</button>
-      <p>Welcome to your new single-page application, built with:</p>
-      <ul>
-        <li>
-          <a href="https://get.asp.net/">ASP.NET Core</a> and{" "}
-          <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
-            C#
-          </a>{" "}
-          for cross-platform server-side code
-        </li>
-        <li>
-          <a href="https://facebook.github.io/react/">React</a> for client-side
-          code
-        </li>
-        <li>
-          <a href="http://getbootstrap.com/">Bootstrap</a> for layout and
-          styling
-        </li>
-      </ul>
-      <p>To help you get started, we have also set up:</p>
-      <ul>
-        <li>
-          <strong>Client-side navigation</strong>. For example, click{" "}
-          <em>Counter</em> then <em>Back</em> to return here.
-        </li>
-        <li>
-          <strong>Development server integration</strong>. In development mode,
-          the development server from <code>create-react-app</code> runs in the
-          background automatically, so your client-side resources are
-          dynamically built on demand and the page refreshes when you modify any
-          file.
-        </li>
-        <li>
-          <strong>Efficient production builds</strong>. In production mode,
-          development-time features are disabled, and your{" "}
-          <code>dotnet publish</code> configuration produces minified,
-          efficiently bundled JavaScript files.
-        </li>
-      </ul>
-      <p>
-        The <code>ClientApp</code> subdirectory is a standard React application
-        based on the <code>create-react-app</code> template. If you open a
-        command prompt in that directory, you can run <code>npm</code> commands
-        such as <code>npm test</code> or <code>npm install</code>.
-      </p>
-    </div>
+      <input onChange={(x) => setRoom(x.target.value)} type="text" />
+      <div />
+      <div />
+      <div />
+      <button onClick={(x) => hub.send(room, message)}>send</button>
+      <button onClick={(x) => hub.create(room)}>create</button>
+      <button onClick={(x) => hub.join(room)}>join</button>
+      <button onClick={(x) => hub.leave(room)}>left</button>
+    </>
   );
 };
